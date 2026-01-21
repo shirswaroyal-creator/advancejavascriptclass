04850bb88cc51b8f97008f1aec0b7f510a429d13
Royal <shirswaroyal@gmail.com>
2026-01-21 12:23:43 +0530

    first commit

---
+++ port/portfolio.html/java.js/javaadv.js
@@ -0,0 +1,30 @@
+// const para=document.getElementsByClassName("para")
+// para[0].innerText="this is a paragraph"
+// para[1].style.color="green"
+//  console.log(para) 
+ 
+// const para=document.querySelectorAll(".para")             //return nodelist
+// para[0].innerText="this is a paragraph"
+// para[1].style.color="green"
+// console.log(para) 
+
+//  let arr=[23,54,46,232,564,23]
+//  arr.forEach((ele)=>console.log(ele))
+ 
+// const container=document.querySelector(".container")
+// container.innerHTML="<h1>this is heading</h1>"
+// console.log(container)
+
+// console.log("document")
+
+
+// const btn=document.querySelector("button")
+// btn.classList.add("btn")
+// btn.addEventListener('click',function(){alert('timepass')
+
+// const container=document.querySelector('.container')
+// const button=document.querySelector('button')
+
+// container.addEventListener('click',()=>{console.log('div')},true)                        //true use hoga to event  capature hoga
+// button.addEventListener('click',()=>{console.log('button')},true)                        // true nhi hai to bubbling hoga
+
