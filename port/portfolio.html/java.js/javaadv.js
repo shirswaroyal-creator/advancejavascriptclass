c43534509a35a4003a7de6b19f363c5a72d1e443
Royal <shirswaroyal@gmail.com>
2026-02-05 11:07:17 +0530

    5-2-26

--- port/portfolio.html/java.js/javaadv.js
+++ port/portfolio.html/java.js/javaadv.js
@@ -27,4 +27,58 @@
 
 // container.addEventListener('click',()=>{console.log('div')},true)                        //true use hoga to event  capature hoga
 // button.addEventListener('click',()=>{console.log('button')},true)                        // true nhi hai to bubbling hoga
+// debugger
+// console.log(b)
+// console.log(a)
 
+// let a=23534
+// var b=5674
+
+// console.log(a)
+// console.log(b)
+
+// function print(){
+//     var c=345
+//     console.log(c)
+//     console.log('inside fn')
+// }
+
+// print()
+
+// setTimeout(()=>{
+//     console.log('line after 5 sec')
+// },10000)
+
+// const timerID=setInterval(()=>{console.log('setinterval')},1000)
+
+// const timeoutID=setTimeout(()=>{clearInterval(timerID)},1000)
+//clear Timeout(timeoutId)
+
+// let count =1;
+// const intervalID = setInterval(function(){
+//     console.log(count);
+//     count++;
+//     if(count>10){
+//         clearInterval(intervalID);
+//     }
+// },1000)
+
+
+const p = new promise ((resolve,reject)=>{
+    let done = true;
+    setTimeout(()=>{
+        if (done){
+            res({name:"royal",age:23})
+        }else{
+            reject("word has not been completed")
+        }
+        //reject()
+    },5000)
+})
+p.then((data)=>{
+    console.log(data.name)
+}).catch((err)=>{
+    console.log(err)
+}).finally(()=>{
+    console.log("finally back")
+})
